8a61a9fcc4482b918dd97b698153e1316cef3ce5
Alec Brazeau <43066394+albrazeau@users.noreply.github.com>
2019-05-09 11:00:19 -0400

    Updated to include an option that reads from s3

--- Raster_BoundingBox_Geometry.py
+++ Raster_BoundingBox_Geometry.py
@@ -6,8 +6,46 @@
 import gdal
 import ogr
 
+# From local
 def tif_enve_to_poly(tif_path):
     src = gdal.Open(tif_path)
+    ulx, xres, xskew, uly, yskew, yres  = src.GetGeoTransform()
+    lrx = ulx + (src.RasterXSize * xres)
+    lry = uly + (src.RasterYSize * yres)
+
+    # Create polygon from bounding box
+    # Create ring
+    ring = ogr.Geometry(ogr.wkbLinearRing)
+    ring.AddPoint(ulx, uly)
+    ring.AddPoint(ulx, lry)
+    ring.AddPoint(lrx,lry)
+    ring.AddPoint(lrx, uly)
+    ring.AddPoint(ulx, uly)
+
+    # Create polygon
+    poly = ogr.Geometry(ogr.wkbPolygon)
+    poly.AddGeometry(ring)
+    
+    return poly
+
+# From s3
+from io import BytesIO
+import boto3
+# one of these needs to be set... not sure which
+#s3 = boto3.resource('s3')
+#s3_client = boto3.client('s3')
+
+def tif_enve_to_poly(s3tif):
+
+# include these three lines if it is an s3 path not an s3 object
+#     bucket_name = s3path.split(r's3://')[1].split(r'/')[0]
+#     key = s3path.split(r'{}/'.format(bucket_name))[1] 
+#     s3tif = s3.Object(bucket_name=bucket_name, key=key)
+    image_data = BytesIO(s3tif.get()['Body'].read())
+    tif_inmem = "/vsimem/data.tif" #Virtual Folder to Store Data
+    gdal.FileFromMemBuffer(tif_inmem, image_data.read())
+    
+    src = gdal.Open(tif_inmem)
     ulx, xres, xskew, uly, yskew, yres  = src.GetGeoTransform()
     lrx = ulx + (src.RasterXSize * xres)
     lry = uly + (src.RasterYSize * yres)
